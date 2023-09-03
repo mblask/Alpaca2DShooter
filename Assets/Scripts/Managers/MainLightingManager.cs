9bcc73b65342186fb524ede239f4ef2ce2ac955f
AlpacaMyGames <mblask2903@gmail.com>
2023-09-03 18:59:11 +0200

    MainLightingManager properties update

--- Assets/Scripts/Managers/MainLightingManager.cs
+++ Assets/Scripts/Managers/MainLightingManager.cs
@@ -5,8 +5,27 @@ public class MainLightingManager : MonoBehaviour
 {
     private static MainLightingManager _instance;
 
-    public static float GlobalIntensity => _instance._mainLight.intensity;
-    public static float MaxIntensity => _instance._mainLightingStrength.y;
+    public static float GlobalIntensity
+    {
+        get
+        {
+            if (_instance == null)
+                return 0.0f;
+
+            return _instance._mainLight.intensity;
+        }
+    }
+
+    public static float MaxIntensity
+    {
+        get
+        {
+            if (_instance == null)
+                return 0.5f;
+
+            return _instance._mainLightingStrength.y;
+        }
+    }
 
     private Light2D _mainLight;
     private Vector2 _mainLightingStrength = new Vector2(0.1f, 0.5f);
